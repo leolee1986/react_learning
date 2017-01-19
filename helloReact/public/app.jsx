1484dc40b8e1ed234a89afe44ccbf4ad603439ce
Li <leo.li@windows.nagrastar.com>
2017-01-19 10:01:51 -0700

    learn createClass(), getDefaultProps

--- helloReact/public/app.jsx
+++ helloReact/public/app.jsx
@@ -1,5 +1,28 @@
 
+// 1st react component, create a var, use React.createClass(), inside it require render method.
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is from the message props'
+    };
+  },
+  render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+    return (
+      <div>
+        <h1>Hello {name}</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Leo';
+
   ReactDOM.render(
-    <h1>Hello React</h1>,
+    // jsx code, render the Greeter from above
+    <Greeter name={firstName}/>,
     document.getElementById('app')
   );
