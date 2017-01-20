7c92f45b9f4be8c8e535e5e9471dd3c2a7934689
leolee1986 <leolee1986@gmail.com>
2017-01-19 22:07:43 -0700

    state and props

--- helloReact/public/app.jsx
+++ helloReact/public/app.jsx
@@ -8,13 +8,35 @@ var Greeter = React.createClass({
       message: 'This is from the message props'
     };
   },
+  getInitialState: function(){
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function (e){
+    e.preventDefault();
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+
+    if(typeof name === 'string' && name.length >0) {
+      this.setState({
+        name: name
+      });
+    }
+  },
   render: function() {
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message;
     return (
       <div>
         <h1>Hello {name}</h1>
         <p>{message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
