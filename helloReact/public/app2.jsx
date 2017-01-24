671622de5b3d21cf0f88b72e9bc028ce2407f1d2
Li <leolee1986@gmail.com>
2017-01-24 09:21:35 -0700

    added comment

--- helloReact/public/app2.jsx
+++ helloReact/public/app2.jsx
@@ -1,3 +1,7 @@
+// mini challenge , create 2 component that nested under 1 contrainer component
+// render on the page, when user update input, it updates screen, don't reflesh
+// the page when submit clicked.
+
 var GreeterMessage2 = React.createClass({
   render: function() {
     var name = this.props.name;
@@ -16,9 +20,11 @@ var GreeterForm2 = React.createClass({
   onFormSubmit: function(e){
     e.preventDefault();
 
+    // create var to store the input data, be sure to use refs not just ref
     var name = this.refs.name.value;
     var message = this.refs.message.value;
 
+    // conditional statement to validate input is not empty
     if(name.length > 0){
       this.refs.name.value = '';
       this.props.onNewName(name);
@@ -31,6 +37,7 @@ var GreeterForm2 = React.createClass({
   },
   render: function(){
     return (
+      // use ref that way the app can reference the data
       <form onSubmit={this.onFormSubmit}>
         <input type="text" ref="name"/>
         <textarea ref="message"></textarea>
@@ -43,23 +50,27 @@ var GreeterForm2 = React.createClass({
 
 
 var Greeter2 = React.createClass({
+  // Default message when start up
   getDefaultProps: function(){
     return {
       name: 'Greeter2',
       message: 'This is from the message props --- Greeter2'
     };
   },
+  // set initial state
   getInitialState: function(){
     return {
       name: this.props.name,
       message: this.props.message
     };
   },
+  // set new name state when user submit form
   handleNewName: function(name) {
     this.setState({
       name: name
     });
   },
+  // set new message state when user submit form
   handleNewMessage: function(message){
     this.setState({
       message : message
@@ -68,6 +79,7 @@ var Greeter2 = React.createClass({
   render: function() {
      var name = this.state.name;
      var message = this.state.message;
+     // render the data
     return (
       <div>
         <GreeterMessage2 name={name} message={message}/>
