34cf0f0fcf1d02d6ed6496e0ed562154e15eef54
leolee1986 <leolee1986@gmail.com>
2017-01-30 22:38:11 -0700

    react boilerplate

---
+++ ReactBoilerplate/webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // by Default, .jsx is not supportted, we will need babel-loader
+  entry: './app/app2.jsx',
+  output: {
+    path:__dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias:{
+
+    },
+    extensions: ['','.js','.jsx']
+  },
+  // webpack loader
+  module: {
+    loaders: [
+      {
+        loader:'babel-loader',
+        query: {
+          presets:['react','es2015']
+        },
+        // which files we want to loader to be applied, use regular express for all .jsx files
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
